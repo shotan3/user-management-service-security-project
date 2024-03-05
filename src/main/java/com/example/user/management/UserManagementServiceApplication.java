6919cf9365413049dbdc758f6b1f777ca43dcd06
shota <shotaandg33@gmail.com>
2024-03-05 16:58:04 +0400

    Initial commit

---
+++ src/main/java/com/example/user/management/UserManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.user.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagementServiceApplication.class, args);
+    }
+
+}
